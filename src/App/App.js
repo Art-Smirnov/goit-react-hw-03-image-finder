a7b7d9ae91a79c2d553d17f8e5a6a05e3b7d0a85
Artem Smirnov <aratatem2013@gmail>
2021-06-02 11:26:49 +0300

    add more functions

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,8 @@ import ImageGallery from '../components/ImageGallery';
 import Modal from '../components/Modal';
 import Button from '../components/Button';
 
+import style from './App.module.scss';
+
 class App extends Component {
   state = {
     showModal: false,
@@ -13,47 +15,59 @@ class App extends Component {
     error: null,
     currentPage: 1,
     query: '',
+    isLoading: false,
   };
 
   // componentDidMount() {}
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchImages();
+    }
+  }
 
   toggleModal = () => {
     this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
 
   onChangeQuery = serchQuery => {
-    this.setState({ query: serchQuery });
-    this.fetchImages();
+    this.setState({ query: serchQuery, currentPage: 1, images: [] });
   };
 
   fetchImages = () => {
     const { currentPage, query } = this.state;
-
+    const options = { query, currentPage };
     imageApi
-      .fetchImagesWithQuery(query, currentPage)
+      .fetchImagesWithQuery(options)
       .then(images =>
         this.setState(prevState => ({
-          images,
+          images: [...prevState.images, ...images],
           currentPage: prevState.currentPage + 1,
         })),
       )
       .catch(error => this.setState({ error }));
+    // .finally(
+    //   window.scrollTo({
+    //     top: document.documentElement.scrollHeight,
+    //     behavior: 'smooth',
+    //   }),
+    // );
   };
 
   render() {
     const { showModal, images, error } = this.state;
     return (
-      <>
+      <div className={style.App}>
         {error && <p>Whoops, something went wrong: {error.message}</p>}
         <Searchbar onSubmit={this.onChangeQuery} />
         <ImageGallery images={images} onOpenModal={this.toggleModal} />
-        <Button onLoadMore={this.fetchImages} />
+        {images.length > 0 && <Button onLoadMore={this.fetchImages} />}
         {showModal && (
           <Modal onCloseModal={this.toggleModal}>
             <img src="" alt="" />
           </Modal>
         )}
-      </>
+      </div>
     );
   }
 }
